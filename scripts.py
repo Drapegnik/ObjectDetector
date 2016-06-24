dc8dfe4fb055edf80e9b2e54185ef6c6c102eac9
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2016-06-24 19:54:03 +0300

    add thresh method swapping depending on background color

--- scripts.py
+++ scripts.py
@@ -2,14 +2,20 @@ import cv2
 
 for num in range(3, 25):
     img = cv2.imread('imgin/example_{0}_1.jpg'.format(num))
-    height, width, channels = img.shape
+    height, width, _ = img.shape
+
+    rect = img[0:50, 0:50]
+    mean = rect.mean()
+    THRESH_METHOD = cv2.THRESH_BINARY if (mean < (255 - mean)) else cv2.THRESH_BINARY_INV
+
     imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(imgray, 127, 255, cv2.THRESH_BINARY_INV)
-    image, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    _, thresh = cv2.threshold(imgray, 127, 255, THRESH_METHOD)
+    _, contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     x1, y1, x2, y2 = width, height, 0, 0
     for con in contours:
         x, y, w, h = cv2.boundingRect(con)
         x1, y1, x2, y2 = min(x, x1), min(y, y1), max(x + w, x2), max(y + h, y2)
+
     img = cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 1)
     cv2.imwrite('imgout/example_{0}_contour.jpg'.format(num), img)
